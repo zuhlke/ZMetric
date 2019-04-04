0a9d49bb7b0a3b58b50b00aa84faebdfd4272c72
Nickson Thanda <nicksonthanda10@msn.com>
2019-04-04 18:14:18 +0100

    ZMETRIC-94 | convert method of DataAdapter can create multiple date entries for a single issue

--- src/CycleTimeReport/DataAdapter.js
+++ src/CycleTimeReport/DataAdapter.js
@@ -4,11 +4,18 @@ export class DataAdapter {
     convert(data) {
         let dateEntries = [];
         for( const entry of data.issues) {
-            dateEntries.push(
-                {
-                    "date": entry.fields.created.split("T")[0],
-                    "averageLeadTime": 0
-                });
+            const startDate = new Date(entry.fields.created);
+            const endDate = new Date(entry.fields.resolutiondate);
+            while (startDate < endDate) {
+                console.log(startDate.toISOString().split('T', 1)[0])
+                dateEntries.push(
+                    {
+                        "date": startDate.toISOString().split('T', 1)[0],
+                        "averageLeadTime": 0
+                    });
+                startDate.setDate(startDate.getDate() + 1);
+
+            }
         }
         return dateEntries;
     }
