5575d8a31495fa8213b475c75eb7b56949e84773
Nickson Thanda <nicksonthanda10@msn.com>
2019-04-04 17:43:25 +0100

    ZMETRIC-94 | refactor DataAdapter to take dynamically create date entries

--- src/CycleTimeReport/DataAdapter.js
+++ src/CycleTimeReport/DataAdapter.js
@@ -2,16 +2,14 @@ export class DataAdapter {
 
 
     convert(data) {
-
-        return [
-            {
-                "date": data.issues[0].fields.created.split("T")[0],
-                "averageLeadTime": 0
-            },
-            {
-                "date": data.issues[1].fields.created.split("T")[0],
-                "averageLeadTime": 0
-            }
-        ];
+        let dateEntries = [];
+        for( const entry of data.issues) {
+            dateEntries.push(
+                {
+                    "date": entry.fields.created.split("T")[0],
+                    "averageLeadTime": 0
+                });
+        }
+        return dateEntries;
     }
 }
