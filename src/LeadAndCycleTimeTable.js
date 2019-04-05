5b3bd6984d08a88ccaaa227423c63479e6279d88
Jason Costa <jasonphilipsardocosta@gmail.com>
2019-04-05 15:52:56 +0100

    ZMETRIC-91: Enable start and end status selection
    Refactor classes to use React Hooks.

--- src/LeadAndCycleTimeTable.js
+++ src/LeadAndCycleTimeTable.js
@@ -1,33 +1,30 @@
-import React, { Component} from 'react';
-import { Table } from 'semantic-ui-react'
+import React from 'react';
+import {Table} from 'semantic-ui-react'
 
-export class LeadAndCycleTimeTable extends Component{
-    render(){
-        return(
-            <div>
-                <h2>ZMetric</h2>
-                <Table celled>
-                    <Table.Header>
-                        <Table.Row>
-                            <Table.HeaderCell>Date</Table.HeaderCell>
-                            <Table.HeaderCell>AverageLeadTime</Table.HeaderCell>
-                            <Table.HeaderCell>AverageCycleTime</Table.HeaderCell>
-                        </Table.Row>
-                    </Table.Header>
-                    <Table.Body>
-                        {
-                            this.props.data.map((currentValue) =>
-                                <Table.Row>
-                                    <Table.Cell>{currentValue.date}</Table.Cell>
-                                    <Table.Cell>{currentValue.averageLeadTime}</Table.Cell>
-                                    <Table.Cell>{currentValue.averageCycleTime}</Table.Cell>
-                                </Table.Row>
-                            )
-                        }
-                    </Table.Body>
-                </Table>
-            </div>
-
-        )
-    }
+export function LeadAndCycleTimeTable(props) {
+    return (
+        <div>
+            <h2>ZMetric</h2>
+            <Table celled>
+                <Table.Header>
+                    <Table.Row>
+                        <Table.HeaderCell>Date</Table.HeaderCell>
+                        <Table.HeaderCell>AverageLeadTime</Table.HeaderCell>
+                        <Table.HeaderCell>AverageCycleTime</Table.HeaderCell>
+                    </Table.Row>
+                </Table.Header>
+                <Table.Body>
+                    {
+                        props.data.map((currentValue) =>
+                            <Table.Row key={currentValue.date}>
+                                <Table.Cell>{currentValue.date}</Table.Cell>
+                                <Table.Cell>{currentValue.averageLeadTime}</Table.Cell>
+                                <Table.Cell>{currentValue.averageCycleTime}</Table.Cell>
+                            </Table.Row>
+                        )
+                    }
+                </Table.Body>
+            </Table>
+        </div>
+    )
 }
--- src/WorkflowContainer.js
+++ src/WorkflowContainer.js
@@ -1,37 +1,80 @@
-import React, { Component} from 'react';
-import { List } from 'semantic-ui-react';
-import { Button } from 'semantic-ui-react';
+import React, {useState} from 'react';
+import {List} from 'semantic-ui-react';
+import {Button} from 'semantic-ui-react';
+import PropTypes from 'prop-types';
+import Label from "semantic-ui-react/dist/commonjs/elements/Label";
 
-export class WorkflowContainer extends Component {
-    constructor(props){
-        super(props);
-    }
-    render(){
-        return(
-            <div>
-                    <List>
-                        <List.Header as='h3'>{this.props.project} Workflow:</List.Header>
-                        {
-                            this.props.workflow.map((status) =>
-                                <List.Item>
-                                    <WorkflowStatus status={status}/>
-                                </List.Item>
-                            )
-                        }
-                    </List>
-            </div>
-        )
-    }
+export function WorkflowContainer(props) {
+    const [startStatus, setStartStatus] = useState(undefined);
+    const [endStatus, setEndStatus] = useState(undefined);
 
+    const setStartOrEndStatus = (statusName) => {
+        if (startStatus === statusName) {
+            setStartStatus(undefined);
+        } else if (startStatus === undefined) {
+            setStartStatus(statusName);
+        } else if (endStatus === statusName) {
+            setEndStatus(undefined);
+        } else {
+            setEndStatus(statusName)
+        }
+    };
+
+    const startOrEndStatus = (statusName) => {
+        if (startStatus === statusName) {
+            return "Clear start status"
+        } else if (startStatus === undefined) {
+            return "Set start status";
+        } else if (endStatus === statusName) {
+            return "Clear end status"
+        } else if (endStatus === undefined) {
+            return "Set end status";
+        } else {
+            return statusName;
+        }
+    };
+
+    const calculateButtonColour = (statusName) => {
+        return statusName === startStatus ? "green" : (statusName === endStatus ? "red" : "grey");
+    };
+
+    return (
+        <div>
+            <Label color={"green"}>Start status: {startStatus}</Label>
+            <br/>
+            <Label color={"red"}>End status: {endStatus}</Label>
+            <List>
+                <List.Header as='h3'>{props.project} Workflow:</List.Header>
+                {
+                    props.workflow.map((status) =>
+                        <List.Item key={status.name}>
+                            <WorkflowStatus status={status} setStartOrEndStatus={setStartOrEndStatus}
+                                            displayOnClickFunction={startOrEndStatus}
+                                            calculateButtonColour={calculateButtonColour}/>
+                        </List.Item>
+                    )
+                }
+            </List>
+        </div>
+    )
 }
 
-export class WorkflowStatus extends Component {
-
-    render(){
-        return(
-            <Button animated>
-                <Button.Content visible>{this.props.status.name}</Button.Content>
-                <Button.Content hidden>Start State</Button.Content>
-            </Button>)
-    }
+function WorkflowStatus(props) {
+    return (
+        <div>
+            <Button size={"large"} animated onClick={() => props.setStartOrEndStatus(props.status.name)}
+                    color={props.calculateButtonColour(props.status.name)}>
+                <Button.Content visible>{props.status.name}</Button.Content>
+                <Button.Content hidden>{props.displayOnClickFunction(props.status.name)}</Button.Content>
+            </Button>
+        </div>);
 }
+
+WorkflowStatus.propTypes = {
+    calculateButtonColour: PropTypes.func.isRequired,
+    setStartOrEndStatus: PropTypes.func.isRequired,
+    displayOnClickFunction: PropTypes.func.isRequired,
+    status: PropTypes.shape({
+        name: PropTypes.string.isRequired
+    })
+};
