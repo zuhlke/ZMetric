706489e13da910c7f35f612e5cff5df2f32380f4
Jason Costa <jasonphilipsardocosta@gmail.com>
2019-04-05 13:49:45 +0100

    Add semantic ui forest themes

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'semantic-ui-css/semantic.min.css'
+import 'semantic-ui-forest-themes/semantic.solar.min.css'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
