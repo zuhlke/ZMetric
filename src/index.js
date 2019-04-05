07fe649dbec5a1dc01b70e66204a4a4df26e63fb
Jason Costa <jasonphilipsardocosta@gmail.com>
2019-04-05 14:10:34 +0100

    Revert "Add semantic ui forest themes"
    This reverts commit 706489e1

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'semantic-ui-css/semantic.min.css'
-import 'semantic-ui-forest-themes/semantic.solar.min.css'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
