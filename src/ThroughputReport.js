6ee332059aba54cb337bad56cccf6c5c992e5c7c
Michael Scott <michaelsco28@gmail.com>
2019-04-10 12:42:57 +0100

    ZMETRIC-98: Refactoring bar graph

--- src/ThroughputReport.js
+++ src/ThroughputReport.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BarChart, Bar, XAxis, YAxis, CartesianGrid} from "recharts";
+import {BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from "recharts";
 
 export function ThroughputReport(props){
 
@@ -8,6 +8,8 @@ export function ThroughputReport(props){
             <CartesianGrid strokeDasharray="3 3" />
             <XAxis lavel={{value:"date", position: "bottom"}} dataKey="date"/>
             <YAxis label={{value:"Throughput (issues)", angle: -90, position: 'insideLeft'}}/>
+            <Tooltip/>
+            <Legend/>
             <Bar dataKey="throughput" fill="#8884d8"/>
         </BarChart>
     )
