d9e6b8b40242c6088d768c2072eb2d1f1582b79d
Nickson Thanda <nicksonthanda10@msn.com>
2019-04-11 10:24:57 +0100

    ZMETRIC-84 | Fix warnings

--- src/App.js
+++ src/App.js
@@ -5,48 +5,6 @@ import {LeadTimeLineChart} from "./LeadTimeLineChart";
 import {WorkflowContainer} from "./WorkflowContainer";
 import {DataAdapter} from "./CycleTimeReport/DataAdapter";
 
-const data = [
-    {
-        "date": "2019-02-01",
-        "averageLeadTime": 0,
-        "averageCycleTime":0
-    },
-    {
-        "date": "2019-02-02",
-        "averageLeadTime": 1,
-        "averageCycleTime": 0,
-    },
-    {
-        "date": "2019-02-03",
-        "averageLeadTime": 1,
-        "averageCycleTime": 1
-    },
-    {
-        "date": "2019-02-04",
-        "averageLeadTime": 3,
-        "averageCycleTime":2
-    },
-    {
-        "date": "2019-02-05",
-        "averageLeadTime": 2,
-        "averageCycleTime": 1
-    },
-    {
-        "date": "2019-02-06",
-        "averageLeadTime": 3,
-        "averageCycleTime": 2
-    },
-    {
-        "date": "2019-02-07",
-        "averageLeadTime": 5,
-        "averageCycleTime": 3
-    },
-    {
-        "date": "2019-02-08",
-        "averageLeadTime": 4,
-        "averageCycleTime": 4
-    }
-];
 
 const jiraResponse = {
     "expand": "schema,names",
