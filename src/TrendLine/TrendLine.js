4a5bd17a8bf2df70bf2bd525df53cde3c180be7a
Michael Scott <michaelsco28@gmail.com>
2019-04-12 14:33:27 +0100

    ZMETRIC-118: Add function to calculate trend line using regressionJS for a given data set.
    4 slightly different implementations were created which are compared to each other in unit tests for two different datasets.

--- src/TrendLine/TrendLine.js
+++ src/TrendLine/TrendLine.js
@@ -2,10 +2,55 @@ import regression from 'regression';
 import moment from "moment";
 
 
-export const convertThroughputDataToTrendLineData = data => generateTrendLineDataSet(convertThroughputToPureData(data));
-const convertThroughputToPureData = data => data.map(entry => [moment(entry.date).unix(), entry.throughput]);
-const generateTrendLineDataSet = data => regression.linear(data).points.map(datapoint => {
-    return {date: moment.unix(datapoint[0]), throughput: datapoint[1]}
-});
+//Unix seconds and high precision linear regression
+export function generateTrendLineTimestamp(data, propertyName) {
+    const dataAsArrayWithTimestamp = data.map(entry => [moment(entry.date).unix(), entry[propertyName]]);
+    const regressionObj = regression.linear(dataAsArrayWithTimestamp, {order: 3, precision:8});
+    const dataSet = regressionObj.points.map(dataPoint => {
+        return {date: moment.unix(dataPoint[0]).toISOString().split('T')[0], [propertyName]: dataPoint[1]}
+    });
+    return dataSet
+}
+
+export function generateTrendLineDays(data, propertyName) {
+    const earliestDate = moment.min(data.map(dataPoint => moment(dataPoint.date)));
+    const dataAsArrayWithDays = data.map( dataPoint => {
+        return [moment(dataPoint.date).diff(earliestDate, "days"), dataPoint[propertyName]]
+    });
+    const regressionObj = regression.linear(dataAsArrayWithDays, {order: 2, precision:3});
+    return regressionObj.points.map( dataPoint => {
+        return {date: earliestDate.clone().add(dataPoint[0], "days").toISOString().split('T')[0], [propertyName]: dataPoint[1]}
+    })
+}
+
+export function generateTrendLineDays2(data, propertyName) {
+    const earliestDate = moment.min(data.map(dataPoint => moment(dataPoint.date)));
+    const dataAsArray = data.map(data => [data.date, data[propertyName]]);
+    const dataAsArrayWithDays = dataAsArray.map( dataPoint => {
+        return [moment(dataPoint[0]).diff(earliestDate, "days"), dataPoint[1]];
+    });
+    const regressionObj = regression.linear(dataAsArrayWithDays, {order: 2, precision:3});
+    return regressionObj.points.map( (dataPoint, index) => {
+        return {date: dataAsArray[index][0], [propertyName]: dataPoint[1]}
+    })
+}
+
+//Should be fastest
+export function generateTrendLineDaysIfDataSetOrderedAndRegularlySpaced(data, propertyName) {
+    const dataAsArray = data.map(data => [data.date, data[propertyName]]);
+    const dataAsArrayWithDays = dataAsArray.map((dataPoint,index) => {
+        return [index, dataPoint[1]]
+    });
+    const regressionObj = regression.linear(dataAsArrayWithDays, {order: 2, precision:3});
+    return regressionObj.points.map( (dataPoint, index) => {
+        return {date: dataAsArray[index][0], [propertyName]: dataPoint[1]}
+    })
+}
+
+
+
+
+
+
 
 
--- src/TrendLine/TrendLine.test.js
+++ src/TrendLine/TrendLine.test.js
@@ -1,12 +1,100 @@
 import {getThroughput} from "../DataFetcher";
-import {convertThroughputDataToTrendLineData} from "./TrendLine";
+import {
+    generateTrendLineDays,
+    generateTrendLineDays2,
+    generateTrendLineDaysIfDataSetOrderedAndRegularlySpaced,
+    generateTrendLineTimestamp,
+} from "./TrendLine";
+
+const throughputData = [
+    {
+        "date": "2019-02-01",
+        "throughput":1
+    },
+    {
+        "date": "2019-02-02",
+        "throughput":1.5
+    },
+    {
+        "date": "2019-02-03",
+        "throughput":3
+    },
+    {
+        "date": "2019-02-04",
+        "throughput":4
+    },
+    {
+        "date": "2019-02-05",
+        "throughput":3
+    },
+    {
+        "date": "2019-02-06",
+        "throughput":6
+    },
+    {
+        "date": "2019-02-07",
+        "throughput":6.7
+    },
+    {
+        "date": "2019-02-08",
+        "throughput":7.7
+    },
+    {
+        "date": "2019-02-09",
+        "throughput":7.2
+    },
+    {
+        "date": "2019-02-10",
+        "throughput":8
+    },
+];
 
 describe("TrendLine", () => {
-    it("returns data set to plot trend line in recharts in correct format for throughput", () => {
-        const initialDataSet = getThroughput();
-        const regressionDataSet = convertThroughputDataToTrendLineData(initialDataSet);
-        console.log(initialDataSet);
-        console.log("\n");
-        console.log( regressionDataSet)
+    it("check two methods for calculating trend line return the same result with mocked data", () => {
+        const trendData = generateTrendLineDays(throughputData, "throughput");
+        const secondTrendData = generateTrendLineTimestamp(throughputData, "throughput");
+        trendData.map( (dataPoint, index) => {
+                return expect(dataPoint.date).toEqual(secondTrendData[index].date) && expect(dataPoint.throughput).toBeCloseTo(secondTrendData[index].throughput);
+        })
+    });
+
+    it("check another pair of methods for calculating trend line return the same result with mocked data", () => {
+        const trendData = generateTrendLineDays(throughputData, "throughput");
+        const secondTrendData = generateTrendLineDays2(throughputData, "throughput");
+        trendData.map( (dataPoint, index) => {
+            return expect(dataPoint.date).toEqual(secondTrendData[index].date) && expect(dataPoint.throughput).toBeCloseTo(secondTrendData[index].throughput);
+        })
+    });
+
+    it("check a third pair of methods for calculating trend line return the same result with mocked data", () => {
+        const trendData = generateTrendLineDaysIfDataSetOrderedAndRegularlySpaced(throughputData, "throughput");
+        const secondTrendData = generateTrendLineTimestamp(throughputData, "throughput");
+        trendData.map( (dataPoint, index) => {
+            return expect(dataPoint.date).toEqual(secondTrendData[index].date) && expect(dataPoint.throughput).toBeCloseTo(secondTrendData[index].throughput);
+        })
+    });
+
+    it( "check two methods for calculating trend line return the same result with real (still mocked atm) data", () => {
+        const trendData = generateTrendLineDays(getThroughput(), "throughput");
+        const secondTrendData = generateTrendLineTimestamp(getThroughput(), "throughput");
+        trendData.map( (dataPoint, index) => {
+            return expect(dataPoint.date).toEqual(secondTrendData[index].date) && expect(dataPoint.throughput).toBeCloseTo(secondTrendData[index].throughput);
+        })
+    });
+
+    it("check another pair of methods for calculating trend line return the same result with real (still mocked atm) data", () => {
+        const trendData = generateTrendLineDays(getThroughput(), "throughput");
+        const secondTrendData = generateTrendLineDays2(getThroughput(), "throughput");
+        trendData.map( (dataPoint, index) => {
+            return expect(dataPoint.date).toEqual(secondTrendData[index].date) && expect(dataPoint.throughput).toBeCloseTo(secondTrendData[index].throughput);
+        })
+    });
+
+    it( "check a third pair of methods for calculating trend line return the same result with real (still mocked atm) data", () => {
+        const trendData = generateTrendLineDaysIfDataSetOrderedAndRegularlySpaced(getThroughput(), "throughput");
+        const secondTrendData = generateTrendLineTimestamp(getThroughput(), "throughput");
+        trendData.map( (dataPoint, index) => {
+            return expect(dataPoint.date).toEqual(secondTrendData[index].date) && expect(dataPoint.throughput).toBeCloseTo(secondTrendData[index].throughput);
+        })
     });
 });
