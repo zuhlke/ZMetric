fde2f68bdf8a5f3cd574a24daa82d22bbcdb2fe4
Michael Scott <michaelsco28@gmail.com>
2019-04-12 16:32:34 +0100

    ZMETRIC-118: Parameterised tests for multiple datasets

--- src/TrendLine/TrendLine.test.js
+++ src/TrendLine/TrendLine.test.js
@@ -50,51 +50,31 @@ const throughputData = [
 ];
 
 describe("TrendLine", () => {
-    it("check two methods for calculating trend line return the same result with mocked data", () => {
-        const trendData = generateTrendLineDays(throughputData, "throughput");
-        const secondTrendData = generateTrendLineTimestamp(throughputData, "throughput");
-        trendData.map( (dataPoint, index) => {
+    const dataSets = [throughputData, getThroughput()];
+    dataSets.forEach( (dataset, index) => {
+
+        it("check two methods for calculating trend line return the same result with dataset " + index + ".", () => {
+            const trendData = generateTrendLineDays(dataset, "throughput");
+            const secondTrendData = generateTrendLineTimestamp(dataset, "throughput");
+            trendData.map( (dataPoint, index) => {
                 return expect(dataPoint.date).toEqual(secondTrendData[index].date) && expect(dataPoint.throughput).toBeCloseTo(secondTrendData[index].throughput);
-        })
-    });
+            })
+        });
 
-    it("check another pair of methods for calculating trend line return the same result with mocked data", () => {
-        const trendData = generateTrendLineDays(throughputData, "throughput");
-        const secondTrendData = generateTrendLineDays2(throughputData, "throughput");
-        trendData.map( (dataPoint, index) => {
-            return expect(dataPoint.date).toEqual(secondTrendData[index].date) && expect(dataPoint.throughput).toBeCloseTo(secondTrendData[index].throughput);
-        })
-    });
+        it("check another pair of methods for calculating trend line return the same result with dataset " + index + ".", () => {
+            const trendData = generateTrendLineDays(dataset, "throughput");
+            const secondTrendData = generateTrendLineDays2(dataset, "throughput");
+            trendData.map( (dataPoint, index) => {
+                return expect(dataPoint.date).toEqual(secondTrendData[index].date) && expect(dataPoint.throughput).toBeCloseTo(secondTrendData[index].throughput);
+            })
+        });
 
-    it("check a third pair of methods for calculating trend line return the same result with mocked data", () => {
-        const trendData = generateTrendLineDaysIfDataSetOrderedAndRegularlySpaced(throughputData, "throughput");
-        const secondTrendData = generateTrendLineTimestamp(throughputData, "throughput");
-        trendData.map( (dataPoint, index) => {
-            return expect(dataPoint.date).toEqual(secondTrendData[index].date) && expect(dataPoint.throughput).toBeCloseTo(secondTrendData[index].throughput);
-        })
-    });
-
-    it( "check two methods for calculating trend line return the same result with real (still mocked atm) data", () => {
-        const trendData = generateTrendLineDays(getThroughput(), "throughput");
-        const secondTrendData = generateTrendLineTimestamp(getThroughput(), "throughput");
-        trendData.map( (dataPoint, index) => {
-            return expect(dataPoint.date).toEqual(secondTrendData[index].date) && expect(dataPoint.throughput).toBeCloseTo(secondTrendData[index].throughput);
-        })
-    });
-
-    it("check another pair of methods for calculating trend line return the same result with real (still mocked atm) data", () => {
-        const trendData = generateTrendLineDays(getThroughput(), "throughput");
-        const secondTrendData = generateTrendLineDays2(getThroughput(), "throughput");
-        trendData.map( (dataPoint, index) => {
-            return expect(dataPoint.date).toEqual(secondTrendData[index].date) && expect(dataPoint.throughput).toBeCloseTo(secondTrendData[index].throughput);
-        })
-    });
-
-    it( "check a third pair of methods for calculating trend line return the same result with real (still mocked atm) data", () => {
-        const trendData = generateTrendLineDaysIfDataSetOrderedAndRegularlySpaced(getThroughput(), "throughput");
-        const secondTrendData = generateTrendLineTimestamp(getThroughput(), "throughput");
-        trendData.map( (dataPoint, index) => {
-            return expect(dataPoint.date).toEqual(secondTrendData[index].date) && expect(dataPoint.throughput).toBeCloseTo(secondTrendData[index].throughput);
-        })
+        it("check a third pair of methods for calculating trend line return the same result with dataset " + index + ".", () => {
+            const trendData = generateTrendLineDaysIfDataSetOrderedAndRegularlySpaced(dataset, "throughput");
+            const secondTrendData = generateTrendLineTimestamp(dataset, "throughput");
+            trendData.map( (dataPoint, index) => {
+                return expect(dataPoint.date).toEqual(secondTrendData[index].date) && expect(dataPoint.throughput).toBeCloseTo(secondTrendData[index].throughput);
+            })
+        });
     });
 });
