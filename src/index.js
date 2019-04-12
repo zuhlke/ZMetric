261872030e3062b648991fc2b093dd922f486da4
Nickson Thanda <nicksonthanda10@msn.com>
2019-04-12 14:50:11 +0100

    ZMETRIC-84 | comment out app import

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import 'semantic-ui-css/semantic.min.css'
-import App from "./App";
+//import App from "./App";
 import Config from "./Config/Config";
 
 ReactDOM.render(<Config />, document.getElementById('root'));
