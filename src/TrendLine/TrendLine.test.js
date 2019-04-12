577ed05c5feb6608ea3f65f7c63c83f93a3b4660
Michael Scott <michaelsco28@gmail.com>
2019-04-12 16:52:19 +0100

    ZMETRIC-118: Reformatted test file

--- src/TrendLine/TrendLine.test.js
+++ src/TrendLine/TrendLine.test.js
@@ -9,72 +9,62 @@ import {
 const throughputData = [
     {
         "date": "2019-02-01",
-        "throughput":1
+        "throughput": 1
     },
     {
         "date": "2019-02-02",
-        "throughput":1.5
+        "throughput": 1.5
     },
     {
         "date": "2019-02-03",
-        "throughput":3
+        "throughput": 3
     },
     {
         "date": "2019-02-04",
-        "throughput":4
+        "throughput": 4
     },
     {
         "date": "2019-02-05",
-        "throughput":3
+        "throughput": 3
     },
     {
         "date": "2019-02-06",
-        "throughput":6
+        "throughput": 6
     },
     {
         "date": "2019-02-07",
-        "throughput":6.7
+        "throughput": 6.7
     },
     {
         "date": "2019-02-08",
-        "throughput":7.7
+        "throughput": 7.7
     },
     {
         "date": "2019-02-09",
-        "throughput":7.2
+        "throughput": 7.2
     },
     {
         "date": "2019-02-10",
-        "throughput":8
+        "throughput": 8
     },
 ];
 
 describe("TrendLine", () => {
     const dataSets = [throughputData, getThroughput()];
-    dataSets.forEach( (dataset, index) => {
+    const functions = [generateTrendLineTimestamp, generateTrendLineDays, generateTrendLineDays2, generateTrendLineDaysIfDataSetOrderedAndRegularlySpaced];
 
-        it("check two methods for calculating trend line return the same result with dataset " + index + ".", () => {
-            const trendData = generateTrendLineDays(dataset, "throughput");
-            const secondTrendData = generateTrendLineTimestamp(dataset, "throughput");
-            trendData.map( (dataPoint, index) => {
-                return expect(dataPoint.date).toEqual(secondTrendData[index].date) && expect(dataPoint.throughput).toBeCloseTo(secondTrendData[index].throughput);
-            })
-        });
+    dataSets.forEach((dataset, dataIndex) => {
 
-        it("check another pair of methods for calculating trend line return the same result with dataset " + index + ".", () => {
-            const trendData = generateTrendLineDays(dataset, "throughput");
-            const secondTrendData = generateTrendLineDays2(dataset, "throughput");
-            trendData.map( (dataPoint, index) => {
-                return expect(dataPoint.date).toEqual(secondTrendData[index].date) && expect(dataPoint.throughput).toBeCloseTo(secondTrendData[index].throughput);
-            })
-        });
-
-        it("check a third pair of methods for calculating trend line return the same result with dataset " + index + ".", () => {
-            const trendData = generateTrendLineDaysIfDataSetOrderedAndRegularlySpaced(dataset, "throughput");
-            const secondTrendData = generateTrendLineTimestamp(dataset, "throughput");
-            trendData.map( (dataPoint, index) => {
-                return expect(dataPoint.date).toEqual(secondTrendData[index].date) && expect(dataPoint.throughput).toBeCloseTo(secondTrendData[index].throughput);
-            })
+        functions.slice(0, functions.length - 1).forEach((fn, index) => {
+            const nextFunction = functions[index + 1];
+            it(fn.name + " and " + nextFunction.name + " return the same result with dataset " + (dataIndex + 1) + ".", () => {
+                const trendData = fn(dataset, "throughput");
+                const secondTrendData = nextFunction(dataset, "throughput");
+                trendData.map((dataPoint, index) => {
+                    return expect(dataPoint.date).toEqual(secondTrendData[index].date) &&
+                        expect(dataPoint.throughput).toBeCloseTo(secondTrendData[index].throughput);
+                })
+            });
         });
     });
 });
