ab8388e3253c8fd9b106f9d415708e9286010f2f
Michael Scott <michaelsco28@gmail.com>
2019-04-12 16:16:03 +0100

    ZMETRIC-118: Modify return type of trend function to also contain original data.
    This is necessary as recharts plots both the original data and the trend line from a single dataset.

--- src/TrendLine/TrendLine.js
+++ src/TrendLine/TrendLine.js
@@ -6,8 +6,8 @@ import moment from "moment";
 export function generateTrendLineTimestamp(data, propertyName) {
     const dataAsArrayWithTimestamp = data.map(entry => [moment(entry.date).unix(), entry[propertyName]]);
     const regressionObj = regression.linear(dataAsArrayWithTimestamp, {order: 3, precision:8});
-    const dataSet = regressionObj.points.map(dataPoint => {
-        return {date: moment.unix(dataPoint[0]).toISOString().split('T')[0], [propertyName]: dataPoint[1]}
+    const dataSet = regressionObj.points.map( (dataPoint,index) => {
+        return {date: moment.unix(dataPoint[0]).toISOString().split('T')[0], [propertyName]: data[index][propertyName], trend: dataPoint[1]}
     });
     return dataSet
 }
@@ -18,8 +18,8 @@ export function generateTrendLineDays(data, propertyName) {
         return [moment(dataPoint.date).diff(earliestDate, "days"), dataPoint[propertyName]]
     });
     const regressionObj = regression.linear(dataAsArrayWithDays, {order: 2, precision:3});
-    return regressionObj.points.map( dataPoint => {
-        return {date: earliestDate.clone().add(dataPoint[0], "days").toISOString().split('T')[0], [propertyName]: dataPoint[1]}
+    return regressionObj.points.map( (dataPoint, index) => {
+        return {date: earliestDate.clone().add(dataPoint[0], "days").toISOString().split('T')[0], [propertyName]: data[index][propertyName], trend: dataPoint[1]}
     })
 }
 
@@ -31,7 +31,7 @@ export function generateTrendLineDays2(data, propertyName) {
     });
     const regressionObj = regression.linear(dataAsArrayWithDays, {order: 2, precision:3});
     return regressionObj.points.map( (dataPoint, index) => {
-        return {date: dataAsArray[index][0], [propertyName]: dataPoint[1]}
+        return {date: dataAsArray[index][0], [propertyName]:  dataAsArray[index][1], trend: dataPoint[1]}
     })
 }
 
@@ -43,10 +43,11 @@ export function generateTrendLineDaysIfDataSetOrderedAndRegularlySpaced(data, pr
     });
     const regressionObj = regression.linear(dataAsArrayWithDays, {order: 2, precision:3});
     return regressionObj.points.map( (dataPoint, index) => {
-        return {date: dataAsArray[index][0], [propertyName]: dataPoint[1]}
+        return {date: dataAsArray[index][0], [propertyName]: dataAsArray[index][1], trend: dataPoint[1]}
     })
 }
 
+export const generateTrendLineData = (data, propertyName) => generateTrendLineDaysIfDataSetOrderedAndRegularlySpaced(data, propertyName);
 
 
 
