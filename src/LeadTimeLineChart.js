a7a004946911c3c74ff6b1397cd20d7d366a3faf
Jason Costa <jasonphilipsardocosta@gmail.com>
2019-04-12 15:12:49 +0100

    ZMETRIC-120: Add dynamic table generation and display

--- src/LeadTimeLineChart.js
+++ src/LeadTimeLineChart.js
@@ -4,40 +4,69 @@ import PropTypes from 'prop-types';
 import {DateRangePicker} from "./DateRangePicker";
 import moment from "moment";
 import {applyDateRangeFilter} from "./DateFiltering/DateFilter";
-import {Label, Segment} from "semantic-ui-react";
+import {Button, Label, Segment, Transition} from "semantic-ui-react";
+import {DynamicTable} from "./DynamicTable";
 
 export function LeadTimeLineChart(props) {
     const [displayedData, updateDisplayedData] = useState(props.data);
+    const [isTableVisible, toggleTableVisibility] = useState(false);
 
     const filterData = dateRange => {
         const newData = applyDateRangeFilter(dateRange, props.data);
         updateDisplayedData(newData)
     };
     return (
-        <Segment.Group horizontal stacked>
-            <Segment>
-                <Label size={'medium'} color='blue' attached='top left'>
-                    Lead Time
-                </Label>
-                <LineChart id="lead-time-line-chart" width={1200} height={400} data={displayedData}>
-                    <Line type="monotone" dataKey="averageLeadTime" stroke="#8884d8"/>
-                    <Line type="monotone" dataKey="averageCycleTime" stroke="#82ca9d"/>
-                    <Tooltip/>
-                    <CartesianGrid strokeDasharray="3 3"/>
-                    <Legend/>
-                    <XAxis dataKey={"date"} label={{value: "date"}}/>
-                    <YAxis
-                        label={{value: "Lead Time Rolling Average (days)", angle: -90, position: 'insideBottomLeft'}}/>
-                </LineChart>
+        <Segment.Group stacked>
+            <Segment.Group horizontal>
+                <Segment>
+                    <Label size={'medium'} color='blue' attached='top left'>
+                        Lead Time
+                    </Label>
+                    <LineChart id="lead-time-line-chart" width={1200} height={400} data={displayedData}>
+                        <Line type="monotone" dataKey="averageLeadTime" stroke="#8884d8"/>
+                        <Line type="monotone" dataKey="averageCycleTime" stroke="#82ca9d"/>
+                        <Tooltip/>
+                        <CartesianGrid strokeDasharray="3 3"/>
+                        <Legend/>
+                        <XAxis dataKey={"date"} label={{value: "date"}}/>
+                        <YAxis
+                            label={{
+                                value: "Lead Time Rolling Average (days)",
+                                angle: -90,
+                                position: 'insideBottomLeft'
+                            }}/>
+                    </LineChart>
+                </Segment>
+                <Segment>
+                    <h4>Select date range:</h4>
+                    {props.data.length &&
+                    <DateRangePicker id={"data-range-picker-lead"} minDate={moment(props.data[0].date)}
+                                     maxDate={moment(props.data[props.data.length - 1].date)}
+                                     dateRangeUpdate={dateRange => filterData(dateRange)}/>
+                    }
+                </Segment>
+            </Segment.Group>
+            <Segment color='green'>
+                <Button
+                    basic
+                    color='green'
+                    content='Data Table'
+                    icon={isTableVisible ? 'arrow circle up ' : 'arrow circle down'}
+                    onClick={() => toggleTableVisibility(!isTableVisible)}
+                    label={{
+                        as: 'a',
+                        basic: true,
+                        color: 'blue',
+                        pointing: 'left',
+                        content: `${props.data.length} entries`
+                    }}
+                />
             </Segment>
-            <Segment>
-                <h4>Select date range:</h4>
-                {props.data.length &&
-                <DateRangePicker id={"data-range-picker-lead"} minDate={moment(props.data[0].date)}
-                                 maxDate={moment(props.data[props.data.length - 1].date)}
-                                 dateRangeUpdate={dateRange => filterData(dateRange)}/>
-                }
-            </Segment>
+            <Transition visible={isTableVisible} animation='fade down' duration={500}>
+                <Segment>
+                    <DynamicTable data={props.data}/>
+                </Segment>
+            </Transition>
         </Segment.Group>
     )
 }
--- src/ThroughputReport.js
+++ src/ThroughputReport.js
@@ -5,12 +5,14 @@ import moment from "moment";
 import {applyDateRangeFilter} from "./DateFiltering/DateFilter";
 import PropTypes from 'prop-types';
 import {calculateAverageThroughput} from "./ThroughputReport/ThroughputDataAdapter";
-import {Label as SemanticLabel, Segment} from "semantic-ui-react";
+import {Button, Label as SemanticLabel, Segment, Transition} from "semantic-ui-react";
+import {DynamicTable} from "./DynamicTable";
 
 export function ThroughputReport(props) {
     const initialAverageThroughput = calculateAverageThroughput(props.data);
     const [displayedData, updateDisplayedData] = useState(props.data);
     const [averageThroughput, updateAverageThroughput] = useState(initialAverageThroughput);
+    const [isTableVisible, toggleTableVisibility] = useState(false);
 
     const filterData = dateRange => {
         const newData = applyDateRangeFilter(dateRange, props.data);
@@ -18,32 +20,55 @@ export function ThroughputReport(props) {
         updateAverageThroughput(calculateAverageThroughput(newData));
     };
     return (
-        <Segment.Group horizontal stacked>
-            <Segment>
-                <SemanticLabel size={'medium'} color='blue' attached='top left'>
-                    Throughput
-                </SemanticLabel>
-                <BarChart width={1200} height={400} data={displayedData} margin={{right: 50}}>
-                    <CartesianGrid strokeDasharray="3 3"/>
-                    <XAxis lavel={{value: "date", position: "bottom"}} dataKey="date"/>
-                    <YAxis label={{value: "Throughput (issues)", angle: -90, position: 'insideLeft'}}/>
-                    <Tooltip/>
-                    <Legend/>
-                    <ReferenceLine y={averageThroughput} stroke="blue" strokeDasharray="3 3">
-                        <Label value="Avg" position="left"/>
-                        <Label value={averageThroughput} position="right"/>
-                    </ReferenceLine>
-                    <Bar dataKey="throughput" fill="#8884d8"/>
-                </BarChart>
+        <Segment.Group stacked>
+            <Segment.Group horizontal>
+                <Segment>
+                    <SemanticLabel size={'medium'} color='blue' attached='top left'>
+                        Throughput
+                    </SemanticLabel>
+                    <BarChart width={1200} height={400} data={displayedData} margin={{right: 50}}>
+                        <CartesianGrid strokeDasharray="3 3"/>
+                        <XAxis lavel={{value: "date", position: "bottom"}} dataKey="date"/>
+                        <YAxis label={{value: "Throughput (issues)", angle: -90, position: 'insideLeft'}}/>
+                        <Tooltip/>
+                        <Legend/>
+                        <ReferenceLine y={averageThroughput} stroke="blue" strokeDasharray="3 3">
+                            <Label value="Avg" position="left"/>
+                            <Label value={averageThroughput} position="right"/>
+                        </ReferenceLine>
+                        <Bar dataKey="throughput" fill="#8884d8"/>
+                    </BarChart>
+                </Segment>
+                <Segment>
+                    <h4>Select date range:</h4>
+                    {props.data.length &&
+                    <DateRangePicker id={"dates-range-picker-throughput"} minDate={moment(props.data[0].date)}
+                                     maxDate={moment(props.data[props.data.length - 1].date)}
+                                     dateRangeUpdate={dateRange => filterData(dateRange)}/>
+                    }
+                </Segment>
+            </Segment.Group>
+            <Segment color='green'>
+                <Button
+                    basic
+                    color='green'
+                    content='Data Table'
+                    icon={isTableVisible ? 'arrow circle up ' : 'arrow circle down'}
+                    onClick={() => toggleTableVisibility(!isTableVisible)}
+                    label={{
+                        as: 'a',
+                        basic: true,
+                        color: 'blue',
+                        pointing: 'left',
+                        content: `${props.data.length} entries`
+                    }}
+                />
             </Segment>
-            <Segment>
-                <h4>Select date range:</h4>
-                {props.data.length &&
-                <DateRangePicker id={"dates-range-picker-throughput"} minDate={moment(props.data[0].date)}
-                                 maxDate={moment(props.data[props.data.length - 1].date)}
-                                 dateRangeUpdate={dateRange => filterData(dateRange)}/>
-                }
-            </Segment>
+            <Transition visible={isTableVisible} animation='fade down' duration={500}>
+                <Segment>
+                    <DynamicTable data={props.data}/>
+                </Segment>
+            </Transition>
         </Segment.Group>
     )
 }
