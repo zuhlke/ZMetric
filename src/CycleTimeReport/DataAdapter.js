a0eaaee9ecdd140d2cde1ef68730b22e1e12049b
Nickson Thanda <nicksonthanda10@msn.com>
2019-04-15 10:54:07 +0100

    ZMETRIC-122 | Fix flaky test

--- src/CycleTimeReport/DataAdapter.js
+++ src/CycleTimeReport/DataAdapter.js
@@ -1,48 +1,42 @@
 import moment from "moment";
 
+export const convert = (data)  => {
+    let dateEntries = [];
+    const periodStartDate = data.issues
+        .map((issue) => new moment(issue.fields.created))
+        .sort((a, b) => a - b)[0];
 
+    const periodEndDate = data.issues
+        .map((issue) => moment.max(new moment(issue.fields.created), issue.fields.resolutiondate ? new moment(issue.fields.resolutiondate) : new moment()))
+        .sort((a, b) => b - a)[0];
 
-export class DataAdapter {
+    const issues = data.issues.map( issue => { return {
+        key: issue.key,
+        creationDate: new moment(issue.fields.created),
+        resolvedDate: issue.fields.resolutiondate ? new moment(issue.fields.resolutiondate) : undefined,
+        leadTime: issue.fields.resolutiondate ? new moment(issue.fields.resolutiondate).diff(new moment(issue.fields.created),'days') : undefined
+    } } ) ;
 
+    while (periodStartDate.diff(periodEndDate) <= 0) {
+        console.log(periodStartDate);
 
+        const currentLeadTime = getAverageLeadTime(issues,periodStartDate);
+        dateEntries.push(
+            {
+                "date": periodStartDate.format('YYYY-MM-DD'),
+                "averageLeadTime": currentLeadTime
+            });
+        periodStartDate.add(1, 'd')
 
-    convert(data) {
-        let dateEntries = [];
-        const periodStartDate = data.issues
-            .map((issue) => new moment(issue.fields.created))
-            .sort((a, b) => a - b)[0];
-
-        const periodEndDate = data.issues
-            .map((issue) => moment.max(new moment(issue.fields.created), issue.fields.resolutiondate ? new moment(issue.fields.resolutiondate) : new moment()))
-            .sort((a, b) => b - a)[0];
-
-        const issues = data.issues.map( issue => { return {
-            key: issue.key,
-            creationDate: new moment(issue.fields.created),
-            resolvedDate: issue.fields.resolutiondate ? new moment(issue.fields.resolutiondate) : undefined,
-            leadTime: issue.fields.resolutiondate ? new moment(issue.fields.resolutiondate).diff(new moment(issue.fields.created),'days') : undefined
-        } } ) ;
-
-
-        while (periodStartDate <= periodEndDate) {
-           const currentLeadTime = DataAdapter.getAverageLeadTime(issues,periodStartDate);
-            dateEntries.push(
-                {
-                    "date": periodStartDate.format('YYYY-MM-DD'),
-                    "averageLeadTime": currentLeadTime
-                });
-            periodStartDate.add(1, 'd')
-        }
-        return dateEntries;
     }
-    static average = arr => arr.length ? arr.reduce( ( l, r ) => l + r, 0 ) / arr.length : 0;
+    return dateEntries;
+};
 
-    static getAverageLeadTime(issues, date) {
-        const leadTimes = issues
-            .filter(issue => issue.resolvedDate && issue.resolvedDate <= date)
-            .map(issue => issue.leadTime);
-        return DataAdapter.average(leadTimes)
-    }
+const average = arr => arr.length ? arr.reduce( ( l, r ) => l + r, 0 ) / arr.length : 0;
 
-
-}
+const getAverageLeadTime = (issues, date) => {
+    const leadTimes = issues
+        .filter(issue => issue.resolvedDate && issue.resolvedDate <= date)
+        .map(issue => issue.leadTime);
+    return average(leadTimes)
+};
--- src/CycleTimeReport/DataAdapter.test.js
+++ src/CycleTimeReport/DataAdapter.test.js
@@ -1,4 +1,4 @@
-import {DataAdapter} from "./DataAdapter";
+import {convert} from "./DataAdapter";
 import moment from "moment";
 
 describe('DataAdapter', () => {
@@ -21,10 +21,10 @@ describe('DataAdapter', () => {
                 }
             ]
         };
-        const result = new DataAdapter().convert(jiraIssue);
+        const result = convert(jiraIssue);
         expect(result).toEqual([{date: "2019-02-08", averageLeadTime: 0}])
     });
-    it('should return two entries when given two jira issue entries on that have been created and resolved on consequtive dates', () => {
+    it('should return two entries when given two jira issue entries on that have been created and resolved on consecutive dates', () => {
         const jiraIssue = {
             "expand": "names,schema",
             "startAt": 0,
@@ -53,7 +53,7 @@ describe('DataAdapter', () => {
                 }
             ]
         };
-        const result = new DataAdapter().convert(jiraIssue);
+        const result = convert(jiraIssue);
         expect(result).toEqual([{date: "2019-02-08", averageLeadTime: 0}, {date: "2019-02-09", averageLeadTime: 0}])
     });
     it('should return the same number of date entries for every date between creation and resolution date of jira issue entry', () => {
@@ -76,12 +76,12 @@ describe('DataAdapter', () => {
             ]
         };
 
-        const result = new DataAdapter().convert(jiraIssue);
+        const result = convert(jiraIssue);
 
         expect(result.length).toEqual(13)
 
     });
-    it('should return the same number of date entries for every date between creation and resolution date of two jira issue entries whos dates are the same', () => {
+    it('should return the same number of date entries for every date between creation and resolution date of two jira issue entries whose dates are the same', () => {
         const jiraIssue = {
             "expand": "names,schema",
             "startAt": 0,
@@ -111,13 +111,13 @@ describe('DataAdapter', () => {
             ]
         };
 
-        const result = new DataAdapter().convert(jiraIssue);
+        const result = convert(jiraIssue);
 
         expect(result.length).toEqual(13)
 
     });
 
-    it('should return the same number of date entries for every date between creation and resolution date of two jira issue entries whos dates overlap', () => {
+    it('should return the same number of date entries for every date between creation and resolution date of two jira issue entries whose dates overlap', () => {
         const jiraIssue = {
             "expand": "names,schema",
             "startAt": 0,
@@ -147,12 +147,12 @@ describe('DataAdapter', () => {
             ]
         };
 
-        const result = new DataAdapter().convert(jiraIssue);
+        const result =  convert(jiraIssue);
         expect(result.length).toEqual(72)
 
     });
 
-    it('should return the same number of date entries for every date between creation and resolution date of two jira issue entries whos dates dont overlap', () => {
+    it('should return the same number of date entries for every date between creation and resolution date of two jira issue entries whose dates don\'t overlap', () => {
         const jiraIssue = {
             "expand": "names,schema",
             "startAt": 0,
@@ -182,7 +182,7 @@ describe('DataAdapter', () => {
             ]
         };
 
-        const result = new DataAdapter().convert(jiraIssue);
+        const result =  convert(jiraIssue);
         expect(result.length).toEqual(72)
 
     });
@@ -201,13 +201,13 @@ describe('DataAdapter', () => {
                     "key": "TRANS-2617",
                     "fields": {
                         "resolutiondate": null,
-                        "created": "2019-04-02T10:15:35.000+0000"
+                        "created": "2019-04-02T00qqqq:15:35.000+0000"
                     }
                 }
             ]
         };
 
-        const result = new DataAdapter().convert(jiraIssue);
+        const result =  convert(jiraIssue);
         expect(result[result.length - 1].date).toEqual(moment().format('YYYY-MM-DD'))
 
     });
@@ -231,7 +231,7 @@ describe('DataAdapter', () => {
             ]
         };
 
-        const result = new DataAdapter().convert(jiraIssue);
+        const result =  convert(jiraIssue);
         expect(result).toEqual([{date: "2019-04-02", averageLeadTime: 0}, {date: "2019-04-03", averageLeadTime: 1}])
 
     });
@@ -250,13 +250,13 @@ describe('DataAdapter', () => {
                     "key": "TRANS-2617",
                     "fields": {
                         "resolutiondate": null,
-                        "created": "2019-04-02T10:15:35.000+0000"
+                        "created": "2019-04-02T00:15:35.000+0000"
                     }
                 }
             ]
         };
 
-        const result = new DataAdapter().convert(jiraIssue);
+        const result =  convert(jiraIssue);
         expect(result[result.length - 1]).toEqual({date: moment().format('YYYY-MM-DD'), averageLeadTime: 0})
 
     });
@@ -275,7 +275,7 @@ describe('DataAdapter', () => {
                     "key": "TRANS-2617",
                     "fields": {
                         "resolutiondate": null,
-                        "created": "2019-04-05T10:15:35.000+0000"
+                        "created": "2019-04-05T00:15:35.000+0000"
                     }
                 },
                 {
@@ -291,7 +291,7 @@ describe('DataAdapter', () => {
             ]
         };
 
-        const result = new DataAdapter().convert(jiraIssue).splice(0,4);
+        const result =  convert(jiraIssue).splice(0,4);
         expect(result).toEqual([{
             date: moment("2019-04-02").format('YYYY-MM-DD'),
             averageLeadTime: 0
@@ -347,7 +347,72 @@ describe('DataAdapter', () => {
             ]
         };
 
-        const result = new DataAdapter().convert(jiraIssue);
+        const result =  convert(jiraIssue);
+        expect(result).toEqual([{
+            date: moment("2019-01-01").format('YYYY-MM-DD'),
+            averageLeadTime: 0
+        }, {
+            date: moment("2019-01-02").format('YYYY-MM-DD'),
+            averageLeadTime: 0
+        }, {
+            date: moment("2019-01-03").format('YYYY-MM-DD'),
+            averageLeadTime: 2
+        }, {
+            date: moment("2019-01-04").format('YYYY-MM-DD'),
+            averageLeadTime: 2
+        }, {
+            date: moment("2019-01-05").format('YYYY-MM-DD'),
+            averageLeadTime: 3
+        }, {
+            date: moment("2019-01-06").format('YYYY-MM-DD'),
+            averageLeadTime: 3
+        }, {
+            date: moment("2019-01-07").format('YYYY-MM-DD'),
+            averageLeadTime: 4
+        }])
+
+    });
+    it('should return an average lead time over time for three issues in the past', () => {
+        const jiraIssue = {
+            "expand": "names,schema",
+            "startAt": 0,
+            "maxResults": 1,
+            "total": 236802,
+            "issues": [
+                {
+                    "expand": "operations,versionedRepresentations,editmeta,changelog,renderedFields",
+                    "id": "1143143",
+                    "self": "https://jira.atlassian.com/rest/api/2/issue/1143143",
+                    "key": "TRANS-2617",
+                    "fields": {
+                        "resolutiondate": "2019-01-03T10:15:35.000+0000",
+                        "created": "2019-01-01T10:15:35.000+0000"
+                    }
+                },
+                {
+                    "expand": "operations,versionedRepresentations,editmeta,changelog,renderedFields",
+                    "id": "1143143",
+                    "self": "https://jira.atlassian.com/rest/api/2/issue/1143143",
+                    "key": "TRANS-2617",
+                    "fields": {
+                        "resolutiondate": "2019-01-05T10:15:35.000+0000",
+                        "created": "2019-01-01T10:15:35.000+0000"
+                    }
+                },
+                {
+                    "expand": "operations,versionedRepresentations,editmeta,changelog,renderedFields",
+                    "id": "1143143",
+                    "self": "https://jira.atlassian.com/rest/api/2/issue/1143143",
+                    "key": "TRANS-2617",
+                    "fields": {
+                        "resolutiondate": "2019-01-07T10:15:35.000+0000",
+                        "created": "2019-01-01T10:15:35.000+0000"
+                    }
+                }
+            ]
+        };
+
+        const result =  convert(jiraIssue);
         expect(result).toEqual([{
             date: moment("2019-01-01").format('YYYY-MM-DD'),
             averageLeadTime: 0
