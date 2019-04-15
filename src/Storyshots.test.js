8baae37b84ae0ee3e172259cf42e0c36188c449c
Vladimir Radzivil <vladimir.radzivil@zuhlke.com>
2019-04-15 17:30:42 +0100

    ZMETRIC-107 | 5% threshold for failure

--- src/Storyshots.test.js
+++ src/Storyshots.test.js
@@ -1,4 +1,14 @@
 import initStoryshots from '@storybook/addon-storyshots';
 import { imageSnapshot } from '@storybook/addon-storyshots-puppeteer';
 
-initStoryshots({ suite: 'Image storyshots', test: imageSnapshot() });
+
+const getMatchOptions = () => ({
+  failureThreshold: 0.05,
+  failureThresholdType: 'percent',
+  fullPage: false,
+  waitUntil: 'networkidle0',
+});
+
+
+
+initStoryshots({ suite: 'Image storyshots', test: imageSnapshot({getMatchOptions}) });
