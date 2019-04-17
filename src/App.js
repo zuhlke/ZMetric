71416254e6ed68158bec38e3e09a26695c3cf6f7
Vladimir Radzivil <vladimir.radzivil@zuhlke.com>
2019-04-17 16:08:37 +0100

    ZMETRIC-84 | Merged from master

--- src/App.js
+++ src/App.js
@@ -1,35 +1,29 @@
 import React, {Component} from 'react';
 import './App.css';
 import {LeadTimeLineChart} from "./LeadTimeLineChart";
-import {WorkflowContainer} from "./WorkflowContainer";
-import {getThroughput, getWorkflow} from "./DataFetcher";
-import {ThroughputReport} from "./ThroughputReport";
 import {Label, Segment} from "semantic-ui-react";
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        console.log(props)
-    }
+  constructor(props) {
+    super(props);
+    console.log(props)
+  }
 
+  render() {
+    return (
+      <div>
+        <Segment.Group raised id={"main-segment"}>
+          <Segment stacked>
+            <Label size={'large'} color='red' ribbon>
+              {this.props.projectName}
+            </Label>
+            <LeadTimeLineChart data={this.props.data}/>
 
-
-
-    render()
-    {
-        return (
-            <div>
-                <Segment.Group raised id={"main-segment"}>
-                    <Segment stacked>
-                        <Label size={'large'} color='red' ribbon>
-                            {this.props.projectName}
-                        </Label>
-                        <LeadTimeLineChart data={this.props.data}/>
-
-                    </Segment>
-                </Segment.Group>
-            </div>
-        );
-    }
+          </Segment>
+        </Segment.Group>
+      </div>
+    );
+  }
 }
+
 export default App;
