50523f112c44f76bfe381a9ab43324195648f86d
vladimir-radzivil <vladimir.radzivil@zuhlke.com>
2019-04-17 14:49:48 +0100

    Zmetric 107 (#17)
    * ZMETRIC-107 | Storybook + Storyshots integration
    
    * ZMETRIC-107 | Run storybook tests as a part of CI
    
    * ZMETRIC-107 | Capture build artifacts
    
    * ZMETRIC-107 | Capture build artifacts
    
    * ZMETRIC-107 | Capture build artifacts
    
    * ZMETRIC-107 | 5% threshold for failure
    
    * ZMETRIC-107 | Storybook static build for CI
    
    * ZMETRIC-107 | Storybook static build for CI
    
    * ZMETRIC-107 | Added http-server
    
    * ZMETRIC-107 | Rearranged CI steps
    
    * ZMETRIC-107 | Rearranged CI steps
    
    * ZMETRIC-107 | Rearranged CI steps
    
    * ZMETRIC-107 | Rearranged CI steps
    
    * ZMETRIC-107 | Rearranged CI steps
    
    * ZMETRIC-107 | Rearranged CI steps

---
+++ src/Storyshots.test.js
@@ -0,0 +1,18 @@
+import initStoryshots from '@storybook/addon-storyshots';
+import {imageSnapshot} from '@storybook/addon-storyshots-puppeteer';
+
+
+const getMatchOptions = () => ({
+  failureThreshold: 0.05,
+  failureThresholdType: 'percent',
+  fullPage: false,
+  waitUntil: 'networkidle0',
+});
+
+
+initStoryshots({
+  suite: 'Image storyshots', test: imageSnapshot({
+    storybookUrl: 'http://localhost:6006',
+    getMatchOptions
+  })
+});
