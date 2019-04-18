de462ff795c7a0b9e3054c6d8d3a8cc343112153
Jason Costa <jasonphilipsardocosta@gmail.com>
2019-04-18 15:23:57 +0100

    ZMETRIC-14: Merge all flows for selected issues

--- src/CumulativeFlowReport/CumulativeFlowReportService.js
+++ src/CumulativeFlowReport/CumulativeFlowReportService.js
@@ -39,3 +39,31 @@ export const toggleProperty = (data, propertyName) => {
     mappedData.set(propertyName, {...mappedData.get(propertyName), selected: !mappedData.get(propertyName).selected});
     return mappedData;
 };
+
+export const mergeTwoDataPoints = (...data) => {
+    return data.reduce((a, b) => {
+        for (let k in b) {
+            if (k === "date") {
+                a[k] = b[k];
+            } else if (b.hasOwnProperty(k)) {
+                a[k] = (a[k] || 0) + b[k];
+            }
+        }
+        return a;
+    }, {});
+};
+
+
+export const mergeData = (data, selectedIssueTypes) => {
+    const filteredFlatData = data.filter(issueType => selectedIssueTypes.has(issueType.name) && selectedIssueTypes.get(issueType.name).selected).flatMap(issueType => issueType.data);
+    const map = new Map();
+    filteredFlatData.forEach(dataPoint => {
+        if (map.has(dataPoint.date)) {
+            map.set(dataPoint.date, mergeTwoDataPoints(dataPoint, map.get(dataPoint.date)));
+        } else {
+            map.set(dataPoint.date, dataPoint);
+        }
+    });
+    return Array.from(map.values());
+};
+
--- src/CumulativeFlowReport/CumulativeFlowReportService.test.js
+++ src/CumulativeFlowReport/CumulativeFlowReportService.test.js
@@ -1,5 +1,5 @@
 import {CumulativeFlowReport} from "./CumulativeFlowReport";
-import {getColoursForNewIssues} from "./CumulativeFlowReportService";
+import {getColoursForNewIssues, mergeData} from "./CumulativeFlowReportService";
 
 describe("CumulativeFlowReport", () => {
     const workflow = [
@@ -31,6 +31,77 @@ describe("CumulativeFlowReport", () => {
         ]);
 
         expect(getColoursForNewIssues(workflow)).toEqual(expectedColours);
+    });
+
+    it('merges data flows for all selected issues', function () {
+        const unmergedData = [
+            {
+                "id": "1",
+                "name": "Bug",
+                "data": [
+                    {
+                        "date": "2019-02-01",
+                        "To Do": 1,
+                        "In Progress": 1,
+                    },
+                    {
+                        "date": "2019-02-02",
+                        "To Do": 1,
+                        "In Progress": 1
+                    }
+                ]
+            }, {
+                "id": "2",
+                "name": "Story",
+                "data": [
+                    {
+                        "date": "2019-02-01",
+                        "To Do": 1,
+                        "In Progress": 1,
+                    },
+                    {
+                        "date": "2019-02-03",
+                        "To Do": 1,
+                        "In Progress": 1
+                    }
+                ]
+            }, {
+                "id": "3",
+                "name": "Epic",
+                "data": [
+                    {
+                        "date": "2019-02-04",
+                        "To Do": 1,
+                        "In Progress": 1,
+                    },
+                    {
+                        "date": "2019-02-05",
+                        "To Do": 1,
+                        "In Progress": 1
+                    }
+                ]
+            }
+        ];
+        const mergedData = [
+            {
+                "date": "2019-02-01",
+                "To Do": 2,
+                "In Progress": 2,
+            },
+            {
+                "date": "2019-02-02",
+                "To Do": 1,
+                "In Progress": 1
+            },
+            {
+                "date": "2019-02-03",
+                "To Do": 1,
+                "In Progress": 1
+            }
+        ];
+        const selectedIssueTypes = new Map([["Bug", {"selected": true}], ["Story", {"selected":true}, ["Epic", {"selected":false}]]]);
+
+        expect(mergeData(unmergedData, selectedIssueTypes)).toEqual(mergedData);
     });
 
 });
