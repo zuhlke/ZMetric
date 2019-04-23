ee766411c4e72ff822f32e06ae9fbf6964464a0a
Nickson Thanda <nicksonthanda10@msn.com>
2019-04-23 17:24:23 +0100

    ZMETRIC-86 | Fix XSRF agent for Elextron

--- public/electron.js
+++ public/electron.js
@@ -10,7 +10,7 @@ let mainWindow;
 
 function createWindow() {
     mainWindow = new BrowserWindow({width: 900, height: 680, webPreferences: { webSecurity: false }});
-    mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
+    mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`,{userAgent: "dummy"});
     mainWindow.on('closed', () => mainWindow = null);
 }
 
