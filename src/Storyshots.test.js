33575784d9817fc2ce5d6b4bc7e7b72f289d76e2
Vladimir Radzivil <vladimir.radzivil@zuhlke.com>
2019-04-26 17:38:50 +0100

    ZMETRIC-84 | Fixed console error problems

--- src/Storyshots.test.js
+++ src/Storyshots.test.js
@@ -2,7 +2,7 @@ import initStoryshots from '@storybook/addon-storyshots';
 import {imageSnapshot} from '@storybook/addon-storyshots-puppeteer';
 
 const getMatchOptions = () => ({
-  failureThreshold: 0.01,
+  failureThreshold: 0.05,
   failureThresholdType: 'percent',
   fullPage: false,
   waitUntil: 'networkidle0',
