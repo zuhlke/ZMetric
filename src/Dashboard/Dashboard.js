8d02dc81af7f92c839c88c122f5853c8940e1d38
Vladimir Radzivil <vladimir.radzivil@zuhlke.com>
2019-05-01 17:53:52 +0100

    ZMETRIC-103 | Editor config applied to all files

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import './Dashboard.css';
 import {LeadTimeLineChart} from "../LeadTimeLineChart";
 import {getCumulativeFlowData} from "../DataFetcher";
@@ -13,38 +13,39 @@ import axios from "axios";
 
 
 export default function Dashboard(props) {
-    const {name, value} = props.session;
-    const {jiraUrl,project} = props;
-    const [workflow, setWorkflow] = useState(undefined);
-    const [leadCycleTimeData, setLeadCycleTimeData] = useState(undefined);
-    const [throughput, setThroughput] = useState(undefined);
+  const {name, value} = props.session;
+  const {jiraUrl, project} = props;
+  const [workflow, setWorkflow] = useState(undefined);
+  const [leadCycleTimeData, setLeadCycleTimeData] = useState(undefined);
+  const [throughput, setThroughput] = useState(undefined);
 
-    const instance = axios.create({baseURL: jiraUrl, headers: {cookie: `${name}=${value}`}});
-    const cumulativeFlowDataMock = getCumulativeFlowData();
-    useEffect(() => {
+  const instance = axios.create({baseURL: jiraUrl, headers: {cookie: `${name}=${value}`}});
+  const cumulativeFlowDataMock = getCumulativeFlowData();
+  useEffect(() => {
     axios
-        .all([instance.get(`rest/api/2/search?maxResults=10000&fields=resolutiondate,created&jql=project=${project}`),
-              instance.get(`/rest/api/2/project/${project}/statuses`)])
-        .then(axios.spread((issueResponse,workflowResponse) =>{
-                setLeadCycleTimeData(convertFromJiraToLeadtime(issueResponse.data));
-                setWorkflow(workflowResponse.data);
-                setThroughput(convertFromJiraToThroughput(issueResponse.data))
+      .all([instance.get(`rest/api/2/search?maxResults=10000&fields=resolutiondate,created&jql=project=${project}`),
+        instance.get(`/rest/api/2/project/${project}/statuses`)])
+      .then(axios.spread((issueResponse, workflowResponse) => {
+          setLeadCycleTimeData(convertFromJiraToLeadtime(issueResponse.data));
+          setWorkflow(workflowResponse.data);
+          setThroughput(convertFromJiraToThroughput(issueResponse.data))
         }
-            ))},[] );
+      ))
+  }, []);
 
-    return (
-        <div>
-            <Segment.Group raised id={"main-segment"}>
-                <Segment stacked>
-                    <Label size={'large'} color='red' ribbon>
-                        {props.project}
-                    </Label>
-                    {workflow && <CumulativeFlowReport data={cumulativeFlowDataMock} workflow={workflow}/>}
-                    {leadCycleTimeData && <LeadTimeLineChart data={leadCycleTimeData}/>}
-                    {throughput && <ThroughputReport data={throughput}/>}
-                    {workflow && <WorkflowContainer workflow={workflow}/>}
-                </Segment>
-            </Segment.Group>
-        </div>
-    );
-  }
+  return (
+    <div>
+      <Segment.Group raised id={"main-segment"}>
+        <Segment stacked>
+          <Label size={'large'} color='red' ribbon>
+            {props.project}
+          </Label>
+          {workflow && <CumulativeFlowReport data={cumulativeFlowDataMock} workflow={workflow}/>}
+          {leadCycleTimeData && <LeadTimeLineChart data={leadCycleTimeData}/>}
+          {throughput && <ThroughputReport data={throughput}/>}
+          {workflow && <WorkflowContainer workflow={workflow}/>}
+        </Segment>
+      </Segment.Group>
+    </div>
+  );
+}
--- src/WorkflowContainer.test.js
+++ src/WorkflowContainer.test.js
@@ -5,131 +5,131 @@ import {mount} from "enzyme";
 
 describe("WorkFlowContainer", () => {
 
-    const workflow =[
+  const workflow = [
+    {
+      "id": "1",
+      "name": "Bug",
+      "statuses": [
         {
-            "id": "1",
-            "name": "Bug",
-            "statuses": [
-                {
-                    "name": "To Do",
-                    "id": "10100"
-                },
-                {
-                    "name": "In Progress",
-                    "id": "3"
-                },
-                {
-                    "name": "Review",
-                    "id": "10202"
-                },
-                {
-                    "name": "Done",
-                    "id": "11803"
-                },
-                {
-                    "name": "On Hold",
-                    "id": "10804"
-                },
-                {
-                    "name": "Ready For Test",
-                    "id": "11903"
-                }
-            ],
+          "name": "To Do",
+          "id": "10100"
+        },
+        {
+          "name": "In Progress",
+          "id": "3"
+        },
+        {
+          "name": "Review",
+          "id": "10202"
+        },
+        {
+          "name": "Done",
+          "id": "11803"
+        },
+        {
+          "name": "On Hold",
+          "id": "10804"
+        },
+        {
+          "name": "Ready For Test",
+          "id": "11903"
+        }
+      ],
 
     },
+    {
+      "id": "10000",
+      "name": "Epic",
+      "statuses": [
         {
-            "id": "10000",
-            "name": "Epic",
-            "statuses": [
-            {
-                "name": "To Do",
-                "id": "10100"
-            },
-            {
-                "name": "In Progress",
-                "id": "3"
-            },
-            {
-                "name": "Review",
-                "id": "10202"
-            },
-            {
-                "name": "Done",
-                "id": "11803"
-            },
-            {
-                "name": "On Hold",
-                "id": "10804"
-            },
-            {
-                "name": "Ready For Review",
-                "id": "12305"
-            }]
-        }
-    ];
+          "name": "To Do",
+          "id": "10100"
+        },
+        {
+          "name": "In Progress",
+          "id": "3"
+        },
+        {
+          "name": "Review",
+          "id": "10202"
+        },
+        {
+          "name": "Done",
+          "id": "11803"
+        },
+        {
+          "name": "On Hold",
+          "id": "10804"
+        },
+        {
+          "name": "Ready For Review",
+          "id": "12305"
+        }]
+    }
+  ];
 
-    it("renders without crashing", () => {
-        const div = document.createElement('div');
-        ReactDOM.render(<WorkflowContainer workflow={workflow} project={"ZMetric"}/>, div);
-        ReactDOM.unmountComponentAtNode(div);
-    });
+  it("renders without crashing", () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<WorkflowContainer workflow={workflow} project={"ZMetric"}/>, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
 
-    it("sets the start status when a status is clicked", () => {
-        const wrapper = mount(<WorkflowContainer workflow={workflow} project={"ZMetric"}/>);
-        const statusButton = wrapper.find('#status-button-3').hostNodes();
-        statusButton.simulate('click');
-        expect(wrapper.find("#start-status").hostNodes().text()).toBe("Start status:  In Progress");
-    });
+  it("sets the start status when a status is clicked", () => {
+    const wrapper = mount(<WorkflowContainer workflow={workflow} project={"ZMetric"}/>);
+    const statusButton = wrapper.find('#status-button-3').hostNodes();
+    statusButton.simulate('click');
+    expect(wrapper.find("#start-status").hostNodes().text()).toBe("Start status:  In Progress");
+  });
 
-    it("sets end status when a second status is clicked", () =>{
-        const wrapper = mount(<WorkflowContainer workflow={workflow} project={"ZMetric"}/>);
-        const statusButton = wrapper.find('#status-button-10100').hostNodes();
-        const secondStatusButton = wrapper.find('#status-button-11803').hostNodes();
-        statusButton.simulate('click');
-        expect(wrapper.find("#start-status").hostNodes().text()).toBe("Start status:  To Do");
-        secondStatusButton.simulate('click');
-        expect(wrapper.find("#end-status").hostNodes().text()).toBe("End status:  Done");
-    });
+  it("sets end status when a second status is clicked", () => {
+    const wrapper = mount(<WorkflowContainer workflow={workflow} project={"ZMetric"}/>);
+    const statusButton = wrapper.find('#status-button-10100').hostNodes();
+    const secondStatusButton = wrapper.find('#status-button-11803').hostNodes();
+    statusButton.simulate('click');
+    expect(wrapper.find("#start-status").hostNodes().text()).toBe("Start status:  To Do");
+    secondStatusButton.simulate('click');
+    expect(wrapper.find("#end-status").hostNodes().text()).toBe("End status:  Done");
+  });
 
-    it("clears the start status when a status is clicked twice", () => {
-        const wrapper = mount(<WorkflowContainer workflow={workflow} project={"ZMetric"}/>);
-        const statusButton = wrapper.find('#status-button-3').hostNodes();
-        statusButton.simulate('click');
-        expect(wrapper.find("#start-status").hostNodes().text()).toBe("Start status:  In Progress");
-        statusButton.simulate('click');
-        expect(wrapper.find("#start-status").hostNodes().text()).toBe("Start status:  ");
-    });
+  it("clears the start status when a status is clicked twice", () => {
+    const wrapper = mount(<WorkflowContainer workflow={workflow} project={"ZMetric"}/>);
+    const statusButton = wrapper.find('#status-button-3').hostNodes();
+    statusButton.simulate('click');
+    expect(wrapper.find("#start-status").hostNodes().text()).toBe("Start status:  In Progress");
+    statusButton.simulate('click');
+    expect(wrapper.find("#start-status").hostNodes().text()).toBe("Start status:  ");
+  });
 
-    it("clears the end status when a status is clicked twice", () => {
-        const wrapper = mount(<WorkflowContainer workflow={workflow} project={"ZMetric"}/>);
-        const statusButton = wrapper.find('#status-button-3').hostNodes();
-        const secondStatusButton = wrapper.find('#status-button-11803').hostNodes();
-        statusButton.simulate('click');
-        expect(wrapper.find("#start-status").hostNodes().text()).toBe("Start status:  In Progress");
-        secondStatusButton.simulate('click');
-        expect(wrapper.find("#end-status").hostNodes().text()).toBe("End status:  Done");
-        secondStatusButton.simulate('click');
-        expect(wrapper.find("#end-status").hostNodes().text()).toBe("End status:  ");
-    });
+  it("clears the end status when a status is clicked twice", () => {
+    const wrapper = mount(<WorkflowContainer workflow={workflow} project={"ZMetric"}/>);
+    const statusButton = wrapper.find('#status-button-3').hostNodes();
+    const secondStatusButton = wrapper.find('#status-button-11803').hostNodes();
+    statusButton.simulate('click');
+    expect(wrapper.find("#start-status").hostNodes().text()).toBe("Start status:  In Progress");
+    secondStatusButton.simulate('click');
+    expect(wrapper.find("#end-status").hostNodes().text()).toBe("End status:  Done");
+    secondStatusButton.simulate('click');
+    expect(wrapper.find("#end-status").hostNodes().text()).toBe("End status:  ");
+  });
 
-    it("changes the start status when one button is clicked twice and then a second is clicked once", () => {
-        const wrapper = mount(<WorkflowContainer workflow={workflow} project={"ZMetric"}/>);
-        const statusButton = wrapper.find('#status-button-3').hostNodes();
-        const secondStatusButton = wrapper.find('#status-button-10804').hostNodes();
-        statusButton.simulate('click');
-        expect(wrapper.find("#start-status").hostNodes().text()).toBe("Start status:  In Progress");
-        statusButton.simulate('click');
-        expect(wrapper.find("#start-status").hostNodes().text()).toBe("Start status:  ");
-        secondStatusButton.simulate('click');
-        expect(wrapper.find("#start-status").hostNodes().text()).toBe("Start status:  On Hold");
-        expect(wrapper.find("#end-status").hostNodes().text()).toBe("End status:  ");
-    });
+  it("changes the start status when one button is clicked twice and then a second is clicked once", () => {
+    const wrapper = mount(<WorkflowContainer workflow={workflow} project={"ZMetric"}/>);
+    const statusButton = wrapper.find('#status-button-3').hostNodes();
+    const secondStatusButton = wrapper.find('#status-button-10804').hostNodes();
+    statusButton.simulate('click');
+    expect(wrapper.find("#start-status").hostNodes().text()).toBe("Start status:  In Progress");
+    statusButton.simulate('click');
+    expect(wrapper.find("#start-status").hostNodes().text()).toBe("Start status:  ");
+    secondStatusButton.simulate('click');
+    expect(wrapper.find("#start-status").hostNodes().text()).toBe("Start status:  On Hold");
+    expect(wrapper.find("#end-status").hostNodes().text()).toBe("End status:  ");
+  });
 
-    it("updates the issue type correctly when an issue type button is clicked", () =>{
-        const wrapper = mount(<WorkflowContainer workflow={workflow} project={"ZMetric"}/>);
-        const issueTypeButton = wrapper.find('#issue-button-10000').hostNodes();
-        issueTypeButton.simulate('click');
-        expect(wrapper.find('#status-button-11903').exists()).toEqual(false);
-        expect(wrapper.find('#status-button-12305').exists()).toEqual(true);
-    })
+  it("updates the issue type correctly when an issue type button is clicked", () => {
+    const wrapper = mount(<WorkflowContainer workflow={workflow} project={"ZMetric"}/>);
+    const issueTypeButton = wrapper.find('#issue-button-10000').hostNodes();
+    issueTypeButton.simulate('click');
+    expect(wrapper.find('#status-button-11903').exists()).toEqual(false);
+    expect(wrapper.find('#status-button-12305').exists()).toEqual(true);
+  })
 });
