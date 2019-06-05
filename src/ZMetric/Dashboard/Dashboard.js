851d2616013fc2e9b41f185ee83a727519de1f81
Vladimir Radzivil <vladimir.radzivil@zuhlke.com>
2019-06-05 18:29:07 +0100

    Removed zMetrics Dashboard

--- src/ZMetric/Dashboard/Dashboard.js
+++ src/ZMetric/Dashboard/Dashboard.js
@@ -1,21 +1,11 @@
-import React, {useState, useEffect} from 'react';
+import React, {useEffect} from 'react';
 import './Dashboard.css';
 import {Label, Segment} from "semantic-ui-react";
 
 import axios from "axios";
-import {ThroughputReport} from "../../Reports/Throughput/ThroughputReport";
-import {LeadTimeLineChart} from "../../Reports/LeadTime/LeadTimeLineChart";
-import {CumulativeFlowReport} from "../../Reports/CumulativeFlow/CumulativeFlowReport";
-import {mergeIssues} from "../../Reports/CumulativeFlow/CumulativeFlowDataAdapter";
-import {convertFromJiraToThroughput} from "../../Reports/Throughput/ThroughputDataAdapter";
-import {convertFromJiraToLeadtime} from "../../Reports/LeadTime/LeadTimeDataAdapter";
 
 
 export default function Dashboard(props) {
-  const [workflow, setWorkflow] = useState(undefined);
-  const [leadCycleTimeData, setLeadCycleTimeData] = useState(undefined);
-  const [throughput, setThroughput] = useState(undefined);
-  const [cumulativeFlow, setCumulativeFlow] = useState(undefined);
 
   useEffect(() => {
     const {jiraUrl, project} = props;
@@ -25,11 +15,8 @@ export default function Dashboard(props) {
     axios
       .all([instance.get(`rest/api/2/search?maxResults=10000&expand=changelog&fields=resolutiondate,created,issuetype&jql=project=${project}`),
         instance.get(`/rest/api/2/project/${project}/statuses`)])
-      .then(axios.spread((issueResponse, workflowResponse) => {
-          setLeadCycleTimeData(convertFromJiraToLeadtime(issueResponse.data));
-          setWorkflow(workflowResponse.data);
-          setThroughput(convertFromJiraToThroughput(issueResponse.data));
-          setCumulativeFlow(mergeIssues(issueResponse.data.issues, workflowResponse.data));
+      .then(axios.spread(() => {
+
         }
       ))
   }, [props]);
@@ -41,9 +28,6 @@ export default function Dashboard(props) {
           <Label size={'large'} color='red' ribbon>
             {props.project}
           </Label>
-          {workflow && cumulativeFlow && <CumulativeFlowReport data={cumulativeFlow} workflow={workflow}/>}
-          {leadCycleTimeData && <LeadTimeLineChart data={leadCycleTimeData}/>}
-          {throughput && <ThroughputReport data={throughput}/>}
         </Segment>
       </Segment.Group>
     </div>
